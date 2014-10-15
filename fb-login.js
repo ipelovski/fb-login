464b8499a49765827364bddc15cfbb73ad23aae3
Ivan Pelovski <ivan.pelovski@gmail.com>
2014-10-15 20:26:41 +0300

    separated the js code into jquery plugins

--- fb-login.js
+++ fb-login.js
@@ -1,100 +1,53 @@
 /* global FB */
-(function () {
+(function ($) {
   "use strict";
 
-  var fbButtonTemplate = '<fb:login-button class="fb-login-button" id="loginbtn" data-size="xlarge" autologoutlink="true" scope="public_profile,email" onlogin="checkLoginState();"></fb:login-button>';
+  // widget for email subscriptions
+  (function () {
+    var StatusClassNames = {
+      'connected': 'status-connected',
+      'not_authorized': 'status-not-authorized',
+      'unknown': 'status-unknown'
+    };
+    var StatusClassNamesArray = ['status-connected', 'status-not-authorized', 'status-unknown'];
+    var fbid, fbEmail;
+    var checkUserUrl = 'http://simulation.dk/other/sumo/checkUser.php';
 
-  
-
-  function widgetsInit(loggedIn) {
-    var lockedContent = $('.locked-content');
-    if (loggedIn) {
-      lockedContent.addClass('unlocked');
-    }
-    else {
-      lockedContent.each(function (idx, elem) {
-        // var container = $(elem);
-        // var widget = container.find('> :first-child');
-        var widget = $(elem);
-        var container = widget.parent();
-        var overlay = $('<div/>').addClass('overlay');
-        overlay.height(widget.height());
-        overlay.width(widget.width());
-        overlay.append(fbButtonTemplate);
-        FB.XFBML.parse(overlay[0]);
-        container.append(overlay);
+    $.fn.subscribe = function () {
+      function initContainer(status) {
+        return function (idx, elem) {
+          var container = $(elem);
+          for (var i = 0; i < StatusClassNamesArray.length; i++) {
+            container.find('.' + StatusClassNamesArray[i]).hide();
+          }
+          var className = StatusClassNames[status];
+          container.find('.' + className).show();
+        };
+      }
+      var self = this;
+      $().fbRepo().on('fbStatusChanged', function (e, response) {
+        self.each(initContainer(response.status)).removeClass('hidden');
       });
+      return this.each(initContainer(''));
+    };
 
-      lockedContent.resize(function (e) {
-        var widget = $(e.target);
-        var container = widget.parent();
-        var offset = container.offset();
-        var height = widget.height();
-        var width = widget.width();
-        var top = offset.top + "px";
-        var left = offset.left + "px";
-
-        container.find('.overlay').css({
-          // 'background-color': 'rgba(0, 0, 0, 0.70)',
-          // 'position': 'absolute',
-          // 'left': left,
-          // 'top': top,
-          'width': width,
-          'height': height,
-          // 'z-index': 100
+    function updateStatusWidget(status) {
+      FB.api('/me?fields=id,email,gender,age_range', function (response) {
+        fbid = response.id;
+        fbEmail = response.email;
+        $.getJSON(checkUserUrl + '?op=0&id=' + fbid, function (data) {
+          var permissionsContainer = $('.permissions-container');
+          permissionsContainer.show();
+          permissionsContainer.find('.newsletter-option').prop('checked', data !== 0);
+          permissionsContainer.find('.offers-option').prop('checked', data !== 0);
         });
-      });//.resize();
+
+        // $.get('checkUser.php?op=1&id=' + fbid + '&val=' + (this.checked ? 1 : 0), function (data) {
+        //   // Something
+        // });
+      });
     }
-  }
-
-  // This is called with the results from from FB.getLoginStatus().
-  function statusChangeCallback(response) {
-
-    $('#permissionContainer').hide();
-
-    //console.log('statusChangeCallback');
-    //console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-      //document.getElementById('loginbtn').style.display = "none";
-      testAPI();
-      
-    } else if (response.status === 'not_authorized') {
-
-      // $('#overlay').show();
-      // document.getElementById('loginspn').innerHTML = '<fb:login-button id="loginbtn" data-size="xlarge" autologoutlink="true" scope="public_profile,email" onlogin="checkLoginState();"></fb:login-button>';
-      // FB.XFBML.parse(document.getElementById('loginspn'));
-
-      // The person is logged into Facebook, but not your app.
-      //document.getElementById('status').innerHTML = 'Please log ' + 'into this app.';
-    } else {
-
-      // $('#overlay').show();
-      // document.getElementById('loginspn').innerHTML = '<fb:login-button id="loginbtn" data-size="xlarge" autologoutlink="true" scope="public_profile,email" onlogin="checkLoginState();"></fb:login-button>';
-      // FB.XFBML.parse(document.getElementById('loginspn'));
-
-
-      // The person is not logged into Facebook, so we're not sure if
-      // they are logged into this app or not.
-      //document.getElementById('status').innerHTML = 'Please log ' + 'into Facebook.';
-    }
-    var loggedIn = response.status === 'connected';
-    widgetsInit(loggedIn);
-  }
-
-  // This function is called when someone finishes with the Login
-  // Button.  See the onlogin handler attached to it in the sample
-  // code below.
-  function checkLoginState() {
-    FB.getLoginStatus(function (response) {
-      statusChangeCallback(response);
-
-    });
-  }
+  }());
 
   window.fbAsyncInit = function () {
     FB.init({
@@ -104,22 +57,6 @@
       xfbml      : true,  // parse social plugins on this page
       version    : 'v2.1', // use version 2.1
       status: true // the SDK will attempt to get info about the current user immediately after init
-    });
-
-    // Now that we've initialized the JavaScript SDK, we call 
-    // FB.getLoginStatus().  This function gets the state of the
-    // person visiting this page and can return one of three states to
-    // the callback you provide.  They can be:
-    //
-    // 1. Logged into your app ('connected')
-    // 2. Logged into Facebook, but not your app ('not_authorized')
-    // 3. Not logged into Facebook and can't tell if they are logged into
-    //    your app or not.
-    //
-    // These three cases are handled in the callback function.
-
-    FB.getLoginStatus(function (response) {
-      statusChangeCallback(response);
     });
   };
 
@@ -132,67 +69,91 @@
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 
-  // Here we run a very simple test of the Graph API after login is
-  // successful.  See statusChangeCallback() for when this call is made.
-  function testAPI() {
-
-    FB.api('/me?fields=id,email,gender,age_range', function (response) {
-
-      // ga('send', 'pageview', {
-      //   'fbgender': response.gender,
-      //   'fbagerange' : response.age_range.min
-      // });
-
-      //console.log(response);
-
-      $.getJSON('checkUser.php?op=0&id=' + response.id, function (data) {
-        $('#overlay').hide();
-        $('#permission').data('fbid', response.id);
-        $('#permissionContainer').show();
-        $('#permission').prop('checked', data !== 0);
-        $('#permission2').prop('checked', data !== 0);
-      });
-    });
-  }
-
-  $('#permission').click(function () {
-    var fbid = $(this).data('fbid');
-    // ga('send', 'event', 'newsletter', 'action', ((this.checked) ? 'signup' : 'cancel'));
-    $.get('checkUser.php?op=1&id=' + fbid + '&val=' + (this.checked ? 1 : 0), function (data) {
-      // Something
-    });
-  });
-
-  $('#permission2').click(function () {
-    var fbid = $(this).data('fbid');
-    // ga( 'send', 'event', 'newsletter', 'action', ( ( this.checked ) ? 'signup' : 'cancel' ) );
-    $.get('checkUser.php?op=1&id=' + fbid + '&val=' + (this.checked ? 1 : 0), function (data) {
-      // Something
-    });
-  });
-
-  $('#permission').click(function () {
-    var fbid = $(this).data('fbid');
-    // ga( 'send', 'event', 'newsletter', 'action', ( ( this.checked ) ? 'signup' : 'cancel' ) );
-    $.get('checkUser.php?op=1&id=' + fbid + '&val=' + (this.checked ? 1 : 0), function (data) {
-      // Something
-    });
-  });
-
-  $('#permission2').click(function () {
-    var fbid = $(this).data('fbid');
-    // ga( 'send', 'event', 'newsletter', 'action', ( ( this.checked ) ? 'signup' : 'cancel' ) );
-    $.get('checkUser.php?op=1&id=' + fbid + '&val=' + (this.checked ? 1 : 0), function (data) {
-      // Something
-    });
-  });
-
-  $(function setLockedContent() {
-    $('.locked').each(function (idx, elem) {
-      var container = $(elem);
-      if (container.find('.locked-content').length === 0) {
-        container.find('> :first-child').addClass('locked-content');
+  // fb repository implemented as a jQuery plugin
+  (function () {
+    var fbRepo = $({});
+    var oldfbAsyncInit = window.fbAsyncInit;
+    function fbStatusChangedHandler(response) {
+      fbRepo.trigger('fbStatusChanged', response);
+    }
+    window.fbAsyncInit = function () {
+      if (typeof oldfbAsyncInit === 'function') {
+        oldfbAsyncInit();
       }
-    });
-  });
-}());
+      FB.getLoginStatus(fbStatusChangedHandler);
+      FB.Event.subscribe('auth.authResponseChange', fbStatusChangedHandler);
+
+      ['auth.login', 'auth.logout', 'auth.authResponseChange', 'auth.statusChange'].forEach(function (eventType) {
+        FB.Event.subscribe(eventType, function (response) {
+          console.log(eventType);
+        });
+      });
+    };
+    $.fn.fbRepo = function () {
+      return fbRepo;
+    };
+  }());
+
+  // overlay widget
+  (function () {
+    // onlogin="checkLoginState();"
+    var fbButtonTemplate = '<fb:login-button class="fb-login-button-xlarge" data-size="xlarge" scope="public_profile,email"></fb:login-button>';
+
+    function widgetsInit(loggedIn) {
+      var lockableContent = $('.lockable-content');
+      var overlays = $('.overlay');
+      if (loggedIn) {
+        lockableContent.addClass('unlockable');
+        overlays.remove();
+      }
+      else {
+        lockableContent.removeClass('unlockable');
+        lockableContent.each(function (idx, elem) {
+          // var container = $(elem);
+          // var widget = container.find('> :first-child');
+          var widget = $(elem);
+          var container = widget.parent();
+          var overlay = $('<div/>').addClass('overlay');
+          overlay.height(widget.height());
+          overlay.width(widget.width());
+          overlay.append(fbButtonTemplate);
+          FB.XFBML.parse(overlay[0]);
+          container.append(overlay);
+        });
+
+        lockableContent.resize(function (e) {
+          var widget = $(e.target);
+          var container = widget.parent();
+          var offset = container.offset();
+          var height = widget.height();
+          var width = widget.width();
+          var top = offset.top + "px";
+          var left = offset.left + "px";
+
+          container.find('.overlay').css({
+            // 'background-color': 'rgba(0, 0, 0, 0.70)',
+            // 'position': 'absolute',
+            // 'left': left,
+            // 'top': top,
+            'width': width,
+            'height': height,
+            // 'z-index': 100
+          });
+        });//.resize();
+      }
+    }
+    $.fn.lockable = function () {
+      $().fbRepo().on('fbStatusChanged', function (e, response) {
+        var loggedIn = response.status === 'connected';
+        widgetsInit(loggedIn);
+      });
+
+      return this.each(function () {
+        var container = $(this);
+        if (container.find('.lockable-content').length === 0) {
+          container.find('> :first-child').addClass('lockable-content');
+        }
+      });
+    };
+  }());
+}(jQuery));
